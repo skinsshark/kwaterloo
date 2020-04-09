33d67c7a9e79482a2b47fb2645bc6668a7c1eaa2
SHARON ZHENG <sharzheng@gmail.com>
2020-04-09 19:53:38 -0400

    add more places

--- data.js
+++ data.js
@@ -4,10 +4,6 @@ const data = [
     "filters": new Set(["food", "top"])
   },
   {
-    "name": "Cameron Chinese Restaurant",
-    "filters": new Set(["food"])
-  },
-  {
     "name": "Zero Waste Bulk",
     "filters": new Set(["shop", "top"])
   },
@@ -18,5 +14,201 @@ const data = [
   {
     "name": "The Yeti Cafe",
     "filters": new Set(["food", "top"])
-  }
+  },
+  {
+    "name": "Matter of Taste Coffee Bar",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Hacienda Coffee",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Special Delivery",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Smile Tiger Coffee Roasters",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Settlement Coffee Roasters",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Seven Shores",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Exotica Cafe",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "brch social",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "The Break Room",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "Canadian Clay and Glass Gallery",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "Kitchener Public Library",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "AMF Frederick",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "Victoria Park",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "Waterloo Park",
+    "filters": new Set(["fun"])
+  },
+  {
+    "name": "Queen Street Yoga",
+    "filters": new Set(["fun", "top"])
+  },
+  {
+    "name": "KEN Sushi House",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "O’My Bakery",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Yummy Chongqing",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Scoop Du Jour",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Bauer Kitchen",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Now Tea",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Mimo Thai Kitchen",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Kim’s Kitchen",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Lancaster Smokehouse",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Pho Dau Bo",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "goodvibes juice co.",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Darlise Cafe",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Kinkaku Izakaya",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Chef On Call",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Red House",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Watami Sushi",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "My-Thai Restaurant",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Loloan Lobby Bar",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Jane Bond",
+    "filters": new Set(["food", "top"])
+  },
+  {
+    "name": "Pho Ben Thanh Restaurant",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Better Chef",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Cameron Chinese",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "Mel’s Diner",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "McDonald’s",
+    "filters": new Set(["food"])
+  },
+  {
+    "name": "A Second Look Books And Movies",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "St. Jacobs Farmers' Market",
+    "filters": new Set(["shop", "food"])
+  },
+  {
+    "name": "T&T Supermarket",
+    "filters": new Set(["shop", "food"])
+  },
+  {
+    "name": "Michaels",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "Luster & Oak",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "Bob's valu-mart",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "Legacy Greens",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "BJ's Photo Labs",
+    "filters": new Set(["shop"])
+  },
+  {
+    "name": "Vincenzo's",
+    "filters": new Set(["shop", "food", "top"])
+  },
+  {
+    "name": "Kitchener Market",
+    "filters": new Set(["shop", "food"])
+  },
 ]
