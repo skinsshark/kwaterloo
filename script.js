16f0a81d6024b7bc61fcf36409127e48f894d973
SHARON ZHENG <sharzheng@gmail.com>
2020-04-09 21:20:50 -0400

    fix navbar bg bug

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const contentContainer = document.querySelector('.content');
 // sections
 let curr = 'all'; // :-(
 
+let rX = Math.ceil(Math.random()*60);
+let rY = Math.ceil(Math.random()*43);
+
 window.addEventListener("scroll", () => {
+
   const scrollY = window.scrollY;
   if (scrollY >= NAV_THRESHOLD) {
     nav.classList.add('stop')
@@ -15,21 +19,20 @@ window.addEventListener("scroll", () => {
     nav.classList.remove('stop')
   }
 
-  if (scrollY % 20 > 15) {
-    const rX = Math.ceil(Math.random()*60);
-    const rY = Math.ceil(Math.random()*43);
+  if (scrollY >= NAV_THRESHOLD || scrollY % 200 > 190) {
+    nav.style.background = `
+      radial-gradient(${rX}vw ${rY}vh at 46vw 50vh, #41E8DE 0%, #FFB72D 100%)
+    `;
+  } else {
+    nav.style.background = 'transparent';
+  }
 
+  if (scrollY % 200 > 190) {
+    rX = Math.ceil(Math.random()*60);
+    rY = Math.ceil(Math.random()*43);
     bg.style.background = `
       radial-gradient(${rX}% ${rY}% at 50% 50%, #41E8DE 0%, #FFB72D 100%)
     `;
-
-    if (scrollY >= NAV_THRESHOLD) {
-      nav.style.background = `
-        radial-gradient(${rX}vw ${rY}vh at 46vw 50vh, #41E8DE 0%, #FFB72D 100%)
-      `;
-    } else {
-      nav.style.background = 'transparent';
-    }
   }
 }, {passive: true});
 
