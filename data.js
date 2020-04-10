8d2dab7f595cb857c7b8e7ae1a045418316471f4
SHARON ZHENG <sharzheng@gmail.com>
2020-04-10 12:48:14 -0400

    add images incl placeholder

--- data.js
+++ data.js
@@ -2,266 +2,266 @@ const data = [
   {
     "name": "Show & Tell Coffee",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0showandtell", "1showandtell", "2showandtell"]
   },
   {
     "name": "Zero Waste Bulk",
     "filters": new Set(["shop", "top"]),
-    "files": []
+    "files": ["0zerobulk"]
   },
   {
     "name": "Princess Cinemas",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0princess"]
   },
   {
     "name": "The Yeti Cafe",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0yeti"]
   },
   {
     "name": "Matter of Taste Coffee Bar",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0mot"]
   },
-  {
-    "name": "Hacienda Coffee",
-    "filters": new Set(["food"]),
-    "files": []
-  },
+  // {
+  //   "name": "Hacienda Coffee",
+  //   "filters": new Set(["food"]),
+  //   "files": [""]
+  // },
   {
     "name": "Special Delivery",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0special"]
   },
   {
     "name": "Smile Tiger Coffee Roasters",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0smiletiger"]
   },
   {
     "name": "Settlement Coffee Roasters",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Seven Shores",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0sevenshores"]
   },
   {
     "name": "Exotica Cafe",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0exotica"]
   },
   {
     "name": "brch social",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0brch"]
   },
   {
     "name": "The Break Room",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0breakroom", "1breakroom"]
   },
   {
     "name": "Canadian Clay and Glass Gallery",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0clay", "1clay", "2clay"]
   },
   {
     "name": "Kitchener Public Library",
     "filters": new Set(["fun"]),
-    "files": [],
+    "files": ["0kpl"],
   },
   {
     "name": "AMF Frederick",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0amf", "1amf", "2amf"]
   },
   {
     "name": "Victoria Park",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0vicpark"]
   },
   {
     "name": "Waterloo Park",
     "filters": new Set(["fun"]),
-    "files": []
+    "files": ["0waterloopark", "1waterloopark", "2waterloopark"]
   },
   {
     "name": "Queen Street Yoga",
     "filters": new Set(["fun", "top"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "KEN Sushi House",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0ken"]
   },
   {
     "name": "O’My Bakery",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0omy"]
   },
   {
     "name": "Yummy Chongqing",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0yummy"]
   },
   {
     "name": "Scoop Du Jour",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0scoop", "1scoop", "2scoop"]
   },
   {
     "name": "Bauer Kitchen",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Now Tea",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Mimo Thai Kitchen",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Kim’s Kitchen",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0kims", "1kims", "2kims"]
   },
   {
     "name": "Lancaster Smokehouse",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0lancaster", "1lancaster", "2lancaster"]
   },
   {
     "name": "Pho Dau Bo",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0phodaubo", "1phodaubo"]
   },
   {
     "name": "goodvibes juice co.",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Darlise Cafe",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0darlise"]
   },
   {
     "name": "Kinkaku Izakaya",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0kinkaku"]
   },
   {
     "name": "Chef On Call",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Red House",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0redhouse", "1redhouse"]
   },
   {
     "name": "Watami Sushi",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0watami", "1watami", "2watami"]
   },
   {
     "name": "My-Thai Restaurant",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Loloan Lobby Bar",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0loloan", "1loloan", "2loloan"]
   },
   {
     "name": "Jane Bond",
     "filters": new Set(["food", "top"]),
-    "files": []
+    "files": ["0janebond", "1janebond", "2janebond"]
   },
   {
     "name": "Pho Ben Thanh Restaurant",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Better Chef",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0betterchef"]
   },
   {
     "name": "Cameron Chinese",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0cameron"]
   },
   {
     "name": "Mel’s Diner",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0mels"]
   },
   {
     "name": "McDonald’s",
     "filters": new Set(["food"]),
-    "files": []
+    "files": ["0mcds", "1mcds", "2mcds"]
   },
   {
     "name": "A Second Look Books And Movies",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["0asecondlook", "1asecondlook"]
   },
   {
     "name": "St. Jacobs Farmers' Market",
     "filters": new Set(["shop", "food"]),
-    "files": []
+    "files": ["0stjacob", "1stjacob"]
   },
   {
     "name": "T&T Supermarket",
     "filters": new Set(["shop", "food"]),
-    "files": []
+    "files": ["0tt", "1tt", "2tt"]
   },
   {
     "name": "Michaels",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Luster & Oak",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["missing"]
   },
   {
     "name": "Bob's valu-mart",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["0bobsvalu", "1bobsvalu","2bobsvalu"]
   },
   {
     "name": "Legacy Greens",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["0legacygreens", "1legacygreens"]
   },
   {
     "name": "BJ's Photo Labs",
     "filters": new Set(["shop"]),
-    "files": []
+    "files": ["0bjphotolabs", "1bjphotolabs"]
   },
   {
     "name": "Vincenzo's",
     "filters": new Set(["shop", "food", "top"]),
-    "files": []
+    "files": ["0vinc"]
   },
   {
     "name": "Kitchener Market",
     "filters": new Set(["shop", "food"]),
-    "files": []
+    "files": ["missing"]
   },
 ]
--- script.js
+++ script.js
@@ -52,14 +52,14 @@ let contents = data;
 
 function renderContent() {
   const shuffledContents = shuffleData(contents);
-  shuffledContents.map((val, i) => {
+  shuffledContents.map((val, i) => {    
     contentContainer.innerHTML += `
       <div class="list-item">
         <p>${val.name}</p>
         <div class="gallery">
-          ${val.files.length > 0 ? `<img src="./assets/${val.files[0]}" />` : ''}
-          ${val.files.length > 1 ? `<img src="./assets/${val.files[1]}" />` : ''}
-          ${val.files.length > 2 ? `<img src="./assets/${val.files[2]}" />` : ''}
+          ${val.files.length > 0 ? `<img src="./assets/${val.files[0]}.png" />` : ''}
+          ${val.files.length > 1 ? `<img src="./assets/${val.files[1]}.png" />` : ''}
+          ${val.files.length > 2 ? `<img src="./assets/${val.files[2]}.png" />` : ''}
         </div>
       </div>
     `;
